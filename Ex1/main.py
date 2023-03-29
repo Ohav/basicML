babefd78fdd5da2bb847e6cf69ee0a835a527450
Ohav Barbi <ohavb@mobileye.com>
2023-03-29 13:02:21 +0300

    Still fixing random sampling

--- Ex1/main.py
+++ Ex1/main.py
@@ -17,28 +17,35 @@ def load_data(path):
     return data
 
 
-
 def get_10percent_cifar():
-    data = load_data(CIFAR_PATH + 'data_batch_1')
-    labels = data[b'labels'][:5000]
-    samples = data[b'data'][:5000] / 255
+    batch = np.random.choice(range(1, 6), 1)[0]
+    data = load_data(CIFAR_PATH + f'data_batch_{batch}')
+    labels = np.random.choice(data[b'labels'], 5000)
+    samples = np.random.choice(data[b'data'], 5000) / 255
     samples = np.moveaxis(samples.reshape(-1, C, H, W), 1, -1)
 
     test_data = load_data(CIFAR_PATH + "test_batch")
-    test_labels = test_data[b"labels"][:1000]
-    test_samples = test_data[b"data"][:1000] / 255
+    test_labels = np.random.choice(test_data[b"labels"], 1000)
+    test_samples = np.random.choice(test_data[b"data"], 1000) / 255
     test_samples = np.moveaxis(test_samples.reshape((-1, C, H, W)), 1, -1)
     return (samples, labels), (test_samples, test_labels)
 
 
 def train_svm():
     (train_X, train_y), (test_X, test_y) = get_10percent_cifar()
-    clf = svm.SVC(kernel='linear')
+    clf = svm.SVC()
 
     clf.fit(train_X.reshape(-1, IMAGE_SIZE), train_y)
 
     y_pred = clf.predict(test_X.reshape(-1, IMAGE_SIZE))
     print("SVM done, recall: {}".format(np.sum(y_pred == test_y) / len(test_y)))
+
+    rbf = svm.SVC(kernel='rbf')
+    rbf.fit(train_X.reshape(-1, IMAGE_SIZE), train_y)
+
+    y_pred = rbf.predict(test_X.reshape(-1, IMAGE_SIZE))
+    print("RBF done, recall: {}".format(np.sum(y_pred == test_y) / test_y))
+
 
 
 
