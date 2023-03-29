eaac8cbc5b740f150d4b4eca3f494d858eb52a56
Ohav Barbi <ohavb@mobileye.com>
2023-03-29 12:47:23 +0300

    Fixed imag ereading. Added SVM

--- Ex1/main.py
+++ Ex1/main.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 CIFAR_PATH = "cifar-10-batches-py/"
+H = W = 32
+C = 3
+IMAGE_SIZE = H * W * C
 
 
 def load_data(path):
@@ -19,30 +22,26 @@ def get_10percent_cifar():
     data = load_data(CIFAR_PATH + 'data_batch_1')
     labels = data[b'labels'][:5000]
     samples = data[b'data'][:5000] / 255
-    samples = samples.reshape((-1, 32, 32, 3))
+    samples = np.moveaxis(samples.reshape(-1, C, H, W), 1, -1)
 
     test_data = load_data(CIFAR_PATH + "test_batch")
     test_labels = test_data[b"labels"][:1000]
     test_samples = test_data[b"data"][:1000] / 255
-    test_samples = test_samples.reshape((-1, 32, 32, 3))
-    plt.imshow(samples[0])
-    plt.show()
-    return (labels, samples), (test_labels, test_samples)
+    test_samples = np.moveaxis(test_samples.reshape((-1, C, H, W)), 1, -1)
+    return (samples, labels), (test_samples, test_labels)
 
 
 def train_svm():
     (train_X, train_y), (test_X, test_y) = get_10percent_cifar()
     clf = svm.SVC(kernel='linear')
 
-    clf.fit(train_X.flatten(), train_y)
+    clf.fit(train_X.reshape(-1, IMAGE_SIZE), train_y)
 
-    y_pred = clf.predict(test_X)
+    y_pred = clf.predict(test_X.reshape(-1, IMAGE_SIZE))
     print("SVM done, recall: {}".format(np.sum(y_pred == test_y) / len(test_y)))
 
 
 
 
 if __name__ == "__main__":
-    get_10percent_cifar()
-    print("Hi")
-    pass
+    train_svm()
