5d2f8d8173c7cdca9256bd0ee07e8551e0ce3373
Yael Shemesh <yael.shemesh9@gmail.com>
2023-03-29 19:39:31 +0300

    part 2 - WIP

--- Ex1/main.py
+++ Ex1/main.py
@@ -4,7 +4,10 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 from consts import *
-
+import torch.optim as optim
+import torch.nn as nn
+from networks import FCN
+import tqdm
 
 
 def load_data(path):
@@ -47,9 +50,58 @@ def train_svm():
     y_test_pred = rbf.predict(test_X)
     print("RBF Accuracy: Train: {} - Test: {}".format(np.sum(y_train_pred == train_y) / len(train_y), np.sum(y_test_pred == test_y) / len(test_y)))
 
+def calc_accuracy_and_loss(net, test_X, test_y, criterion):
+    outputs = net(test_X)
+    # the class with the highest energy is what we choose as prediction
+    _, predicted = torch.max(outputs.data, 1)
+    total = test_y.size(0)
+    correct = (predicted == test_y).sum().item()
+    accuracy = correct/total
+    loss = criterion(outputs, test_y)
+    return accuracy, loss
 
+def train_nn(criterion=nn.CrossEntropyLoss(), lr=0.001, momentum=0.9, std=0.1, number_of_epochs=50):
+    (train_X, train_y), (test_X, test_y) = get_10percent_cifar()
+    train_X = train_X.reshape(-1, CIFAR_IMAGE_SIZE)
+    test_X = test_X.reshape(-1, CIFAR_IMAGE_SIZE)
+    train_X = torch.tensor(train_X, dtype=torch.float32)
+    train_y = torch.tensor(train_y, dtype=torch.long)
+    test_X = torch.tensor(test_X, dtype=torch.float32)
+    test_y = torch.tensor(test_y, dtype=torch.long)
+    net = FCN(256)
+    net.init_weights(std)
+    losses = np.zeros(number_of_epochs)
+    optimizer = optim.SGD(net.parameters(), lr=lr, momentum=momentum)
+    for epoch in tqdm.tqdm(range(number_of_epochs)):  # loop over the dataset multiple times
+        running_loss = 0.0
+        for i in range(0, len(train_X), 64):
+            inputs = train_X[i:i + 64]
+            labels = train_y[i:i + 64]
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+        losses[epoch] = running_loss
 
+    return calc_accuracy_and_loss(net, test_X, test_y, criterion)
 
+def grid_search():
+    results = {}
+    min_loss = 10000000
+    min_params = ()
+    for lr in [5e-5, 1e-4, 5e-4, 1e-3, 5e-3]:
+        for momentum in [0.8, 0.85, 0.9, 0.95]:
+            for std in [0.001, 0.01, 0.1, 0.5, 1]:
+                acc, loss = train_nn(criterion=nn.CrossEntropyLoss(), lr=lr, momentum=momentum, std=std, number_of_epochs=10)
+                results[(lr, momentum, std)] = (acc, loss)
+                print(f"The trio {lr}, {momentum}, {std} got a loss/acc: {loss}/{acc}")
+                if loss < min_loss:
+                    min_params = (lr, momentum, std)
+    print(results)
+    print(f"Best: {min_loss} made by {min_params}")
 
 if __name__ == "__main__":
-    train_svm()
+    #train_svm()
+    print(grid_search())
--- Ex1/networks.py
+++ Ex1/networks.py
@@ -1,16 +1,23 @@
 import torch
 import torch.nn as nn
 from consts import CIFAR_IMAGE_SIZE, CIFAR_CLASS_COUNT
+import torch.nn.functional as F
 
 
 class FCN(nn.Module):
     def __init__(self, hidden_width):
-        super(nn.Module).__init__()
+        super().__init__()
         self.layer1 = nn.Linear(CIFAR_IMAGE_SIZE, hidden_width)
         self.out = nn.Linear(hidden_width, CIFAR_CLASS_COUNT)
 
+    def init_weights(self, std):
+        self.layer1.weight.data.normal_(mean=0.0, std=std)
+        self.layer1.bias.data.normal_(mean=0.0, std=std)
+        self.out.weight.data.normal_(mean=0.0, std=std)
+        self.out.bias.data.normal_(mean=0.0, std=std)
+
     def forward(self, X):
-        first_layer = self.layer1(X)
+        first_layer = F.relu(self.layer1(X))
         result = self.out(first_layer)
         return result
 
