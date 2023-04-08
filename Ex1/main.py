5ac96b4b2b6a49762612b34273e59f00255cde6f
Yael Shemesh <yael.shemesh9@gmail.com>
2023-04-08 11:02:18 +0300

    part 3 - WIP

--- Ex1/main.py
+++ Ex1/main.py
@@ -6,7 +6,7 @@ import numpy as np
 from consts import *
 import torch.optim as optim
 import torch.nn as nn
-from networks import FCN
+from networks import FCN, CNN
 import tqdm
 
 
@@ -23,13 +23,13 @@ def get_10percent_cifar():
     train_idx = np.random.choice(len(data[b'labels']), 5000, replace=False)
     labels = np.array(data[b'labels'])[train_idx]
     samples = np.array(data[b'data'])[train_idx] / 255
-    samples = np.moveaxis(samples.reshape(-1, C, H, W), 1, -1)
+    samples = samples.reshape(-1, C, H, W)
 
     test_data = load_data(CIFAR_PATH + "test_batch")
     test_idx = np.random.choice(len(test_data[b'labels']), 1000, replace=False)
     test_labels = np.array(test_data[b"labels"])[test_idx]
     test_samples = np.array(test_data[b"data"])[test_idx] / 255
-    test_samples = np.moveaxis(test_samples.reshape((-1, C, H, W)), 1, -1)
+    test_samples = test_samples.reshape((-1, C, H, W))
     return (samples, labels), (test_samples, test_labels)
 
 
@@ -61,22 +61,23 @@ def calc_accuracy_and_loss(net, test_X, test_y, criterion):
     return accuracy, loss.detach().numpy().reshape(1)[0]
 
 
-def train_nn(criterion=nn.CrossEntropyLoss(), lr=0.001, momentum=0.9, std=0.1, number_of_epochs=50, optimizer='sgd', init='normal'):
+def train_nn(network=FCN, criterion=nn.CrossEntropyLoss(), lr=0.001, momentum=0.9, std=0.1, number_of_epochs=50, optimizer='sgd', init='normal'):
     (train_X, train_y), (test_X, test_y) = get_10percent_cifar()
-    train_X = train_X.reshape(-1, CIFAR_IMAGE_SIZE)
-    test_X = test_X.reshape(-1, CIFAR_IMAGE_SIZE)
+    # train_X = train_X.reshape(-1, CIFAR_IMAGE_SIZE)
+    # test_X = test_X.reshape(-1, CIFAR_IMAGE_SIZE)
     train_X = torch.tensor(train_X, dtype=torch.float32)
     train_y = torch.tensor(train_y, dtype=torch.long)
     test_X = torch.tensor(test_X, dtype=torch.float32)
     test_y = torch.tensor(test_y, dtype=torch.long)
-    net = FCN(256)
+    net = network()
     if init == 'normal':
         net.init_weights(std)
     elif init == 'xavier':
         net.init_weights_xavier()
-    losses = np.zeros(number_of_epochs)
     test_loss = []
     test_acc = []
+    train_loss = []
+    train_acc = []
     if optimizer == "sgd":
         optimizer = optim.SGD(net.parameters(), lr=lr, momentum=momentum)
     elif optimizer == "adam":
@@ -85,7 +86,6 @@ def train_nn(criterion=nn.CrossEntropyLoss(), lr=0.001, momentum=0.9, std=0.1, n
         print("Invalid optimizer " + str(optimizer))
         return 0
     for epoch in tqdm.tqdm(range(number_of_epochs)):  # loop over the dataset multiple times
-        running_loss = 0.0
         indices = list(range(len(train_X)))
         np.random.shuffle(indices)
         train_X = train_X[indices]
@@ -98,81 +98,88 @@ def train_nn(criterion=nn.CrossEntropyLoss(), lr=0.001, momentum=0.9, std=0.1, n
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
-            running_loss += loss.item()
-        losses[epoch] = running_loss
-        stats = calc_accuracy_and_loss(net, test_X, test_y, criterion)
-        test_loss.append(stats[1])
-        test_acc.append(stats[0])
+        test_stats = calc_accuracy_and_loss(net, test_X, test_y, criterion)
+        test_loss.append(test_stats[1])
+        test_acc.append(test_stats[0])
+        train_stats = calc_accuracy_and_loss(net, train_X, train_y, criterion)
+        train_loss.append(train_stats[1])
+        train_acc.append(train_stats[0])
 
-    return test_loss, test_acc
+    return (test_loss, test_acc), (train_loss, train_acc)
 
-def grid_search():
+def grid_search(network=FCN):
     results = {}
     min_loss = 10000000
     min_params = ()
     for lr in [5e-5, 1e-4, 5e-4, 1e-3, 5e-3]:
         for momentum in [0.8, 0.85, 0.9, 0.95]:
             for std in [0.001, 0.01, 0.1, 0.5, 1]:
-                acc, loss = train_nn(criterion=nn.CrossEntropyLoss(), lr=lr, momentum=momentum, std=std, number_of_epochs=10)
+                test_stats, train_stats = train_nn(network=network, criterion=nn.CrossEntropyLoss(), lr=lr, momentum=momentum, std=std, number_of_epochs=10)
+                loss, acc = test_stats
+                loss = loss[-1]
+                acc = acc[-1]
                 results[(lr, momentum, std)] = (acc, loss)
                 print(f"The trio {lr}, {momentum}, {std} got a loss/acc: {loss}/{acc}")
                 if loss < min_loss:
                     min_params = (lr, momentum, std)
+                    min_loss = loss
     print(results)
     print(f"Best: {min_loss} made by {min_params}")
 
 
-def compare_sgd_adam():
-    # Q2.2
-    # Run SGD and ADAM and plot accuracies & loss.
-    epoch_count = 20
-    sgd_stats = train_nn(criterion=nn.CrossEntropyLoss(), lr=5e-3, momentum=0.95, std=0.001, number_of_epochs=epoch_count, optimizer='sgd')
-    adam_stats = train_nn(criterion=nn.CrossEntropyLoss(), lr=5e-3, momentum=0.95, number_of_epochs=epoch_count, optimizer='adam')
+def plot_two_compared_configuration_stats(stat1, stat2, name1, name2, epoch_count):
     plt.figure()
     plt.subplot(121)
-    plt.title("SGD vs Adam test set loss, over epochs")
-    plt.plot(range(epoch_count), sgd_stats[0], 'b', label="SGD loss")
-    plt.plot(range(epoch_count), adam_stats[0], 'r', label='Adam loss')
+    test_stats1, train_stats1 = stat1
+    test_stats2, train_stats2 = stat2
+
+    plt.title(f"{name1} vs {name2} test & train loss, over epochs")
+    plt.plot(range(epoch_count), test_stats1[0], 'b', label=f"{name1} test loss")
+    plt.plot(range(epoch_count), test_stats2[0], 'r', label=f'{name2} test loss')
+    plt.plot(range(epoch_count), train_stats1[0], 'b--', label=f"{name1} train loss")
+    plt.plot(range(epoch_count), train_stats2[0], 'r--', label=f'{name2} train loss')
+
     plt.legend()
     plt.xlabel("Epoch Number")
     plt.ylabel("Test loss")
     plt.subplot(122)
-    plt.title("SGD vs Adam test set accuracy, over epochs")
-    plt.plot(range(epoch_count), sgd_stats[1], 'b', label="SGD accuracy")
-    plt.plot(range(epoch_count), adam_stats[1], 'r', label='Adam accuracy')
+    plt.title(f"{name1} vs {name2} test set accuracy, over epochs")
+    plt.plot(range(epoch_count), test_stats1[1], 'b', label=f"{name1} test accuracy")
+    plt.plot(range(epoch_count), test_stats2[1], 'r', label=f'{name2} test accuracy')
+    plt.plot(range(epoch_count), train_stats1[1], 'b--', label=f"{name1} train accuracy")
+    plt.plot(range(epoch_count), train_stats2[1], 'r--', label=f'{name2} train accuracy')
     plt.legend()
     plt.xlabel("Epoch Number")
     plt.ylabel("Test accuracy")
     plt.show()
 
-def xavier_init():
+
+def compare_sgd_adam(network=FCN, lr=5e-3, momentum=0.95, std=0.001):
+    # Q2.2
+    # Run SGD and ADAM and plot accuracies & loss.
+    epoch_count = 50
+    sgd_stats = train_nn(network=network, criterion=nn.CrossEntropyLoss(), lr=lr, momentum=momentum,
+                         std=std, number_of_epochs=epoch_count, optimizer='sgd')
+    adam_stats = train_nn(network=network, criterion=nn.CrossEntropyLoss(), lr=lr, momentum=momentum,
+                          std=std, number_of_epochs=epoch_count, optimizer='adam')
+    plot_two_compared_configuration_stats(sgd_stats, adam_stats, 'SGD', 'Adam', epoch_count)
+
+def xavier_init(network=FCN, lr=5e-3, momentum=0.95, std=0.01):
     # Q2.3
     # Run SGD with std init and Xavier init and plot accuracies & loss.
-    epoch_count = 20
-    sgd_stats = train_nn(criterion=nn.CrossEntropyLoss(), lr=5e-3, momentum=0.95, std=0.001,
-                         number_of_epochs=epoch_count, optimizer='sgd')
-    xavier_stats = train_nn(criterion=nn.CrossEntropyLoss(), lr=5e-3, momentum=0.95,
-                          number_of_epochs=epoch_count, optimizer='sgd', init='xavier')
-    plt.figure()
-    plt.subplot(121)
-    plt.title("Normal vs Xavier init SGD test set loss, over epochs")
-    plt.plot(range(epoch_count), sgd_stats[0], 'b', label="Normal Distribution loss")
-    plt.plot(range(epoch_count), xavier_stats[0], 'r', label='Xavier loss')
-    plt.legend()
-    plt.xlabel("Epoch Number")
-    plt.ylabel("Test loss")
-    plt.subplot(122)
-    plt.title("Normal vs Xavier init SGD test set accuracy, over epochs")
-    plt.plot(range(epoch_count), sgd_stats[1], 'b', label="Normal Distribution accuracy")
-    plt.plot(range(epoch_count), xavier_stats[1], 'r', label='Xavier accuracy')
-    plt.legend()
-    plt.xlabel("Epoch Number")
-    plt.ylabel("Test accuracy")
-    plt.show()
+    epoch_count = 50
+    normal_stats = train_nn(network=network, criterion=nn.CrossEntropyLoss(), lr=lr, momentum=momentum,
+                            std=std, number_of_epochs=epoch_count, optimizer='sgd')
+    xavier_stats = train_nn(network=network, criterion=nn.CrossEntropyLoss(), lr=lr, momentum=momentum,
+                            number_of_epochs=epoch_count, optimizer='sgd', init='xavier')
+    plot_two_compared_configuration_stats(normal_stats, xavier_stats, 'Normal', 'Xaviar', epoch_count)
 
 
 if __name__ == "__main__":
     #train_svm()
-    # print(grid_search())
+    # grid_search(FCN)
     # compare_sgd_adam()
-    xavier_init()
+    # xavier_init()
+    # grid_search(CNN)
+    #compare_sgd_adam(network=CNN, lr=5e-3, momentum=0.8, std=0.1)
+    xavier_init(network=CNN, lr=5e-3, momentum=0.8, std=0.1)
--- Ex1/networks.py
+++ Ex1/networks.py
@@ -6,11 +6,11 @@ import numpy as np
 
 
 class FCN(nn.Module):
-    def __init__(self, hidden_width):
+    def __init__(self):
         super().__init__()
-        self.hidden_width = hidden_width
-        self.layer1 = nn.Linear(CIFAR_IMAGE_SIZE, hidden_width)
-        self.out = nn.Linear(hidden_width, CIFAR_CLASS_COUNT)
+        self.hidden_width = 256
+        self.layer1 = nn.Linear(CIFAR_IMAGE_SIZE, self.hidden_width)
+        self.out = nn.Linear(self.hidden_width, CIFAR_CLASS_COUNT)
 
     def init_weights(self, std):
         self.layer1.weight.data.normal_(mean=0.0, std=std)
@@ -27,9 +27,50 @@ class FCN(nn.Module):
         self.out.bias.data.uniform_(-out_range, out_range)
 
     def forward(self, X):
+        X = torch.flatten(X, 1)  # flatten all dimensions except batch
         first_layer = F.relu(self.layer1(X))
         result = self.out(first_layer)
         return result
+
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 64, 3)  # output - 30 * 30 * 64 (H x W x D)
+        self.pool1 = nn.MaxPool2d(2, 2)  # output - 15 * 15 * 64
+        self.conv2 = nn.Conv2d(64, 16, 3)  # output - 13 * 13 * 16
+        self.pool2 = nn.MaxPool2d(2, 2)  # output - 7 * 7 * 16
+
+        self.fc = nn.Linear(6*6*16, 784)
+        self.out = nn.Linear(784, 10)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        x = self.fc(x)
+        x = self.out(x)
+        return x
+
+    def init_weights(self, std):
+        self.conv1.weight.data.normal_(mean=0.0, std=std)
+        self.conv1.bias.data.normal_(mean=0.0, std=std)  # TODO: do convolutional layers have bias?
+        self.conv2.weight.data.normal_(mean=0.0, std=std)
+        self.conv2.bias.data.normal_(mean=0.0, std=std)
+        self.fc.weight.data.normal_(mean=0.0, std=std)
+        self.fc.bias.data.normal_(mean=0.0, std=std)
+        self.out.weight.data.normal_(mean=0.0, std=std)
+        self.out.bias.data.normal_(mean=0.0, std=std)
+
+    def init_weights_xavier(self):
+        nn.init.xavier_uniform_(self.conv1.weight)
+        #nn.init.xavier_uniform_(self.conv1.bias)
+        nn.init.xavier_uniform_(self.conv2.weight)
+        #nn.init.xavier_uniform_(self.conv2.bias)
+        nn.init.xavier_uniform_(self.fc.weight)
+        #nn.init.xavier_uniform_(self.fc.bias)
+        nn.init.xavier_uniform_(self.out.weight)
+        #nn.init.xavier_uniform_(self.out.bias)
+
 
 
 
